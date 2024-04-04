e2dc0178cc9a5ed08ff9a0378bea4a63299d7ce9
Har0n <144205713+Har0n313@users.noreply.github.com>
2024-04-04 15:00:28 +0300

    Initial commit

---
+++ Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application.Interfaces
+{
+    public interface IRepository <TEntity>
+    {
+        public TEntity GetById(Guid id);
+        public List<TEntity> Get();
+        public TEntity Create(TEntity person);
+        public TEntity Update(TEntity person);
+        public bool Delete(Guid id);
+    }
+}
---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// Базовый класс для сущностей
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj is not BaseEntity entity) return false;
+            if (Id != entity.Id) return false;
+            return this.GetHashCode() == entity.GetHashCode(); 
+            //referencequals
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
